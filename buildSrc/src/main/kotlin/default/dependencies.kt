ba2cdd03553e3a90a27e691a1970d688d5c33720
Christopher Santana <christopher.santana@nav.no>
2025-03-31 14:22:07 +0200

    Tester versjon 2.1.1 av kafka-lib.

--- buildSrc/src/main/kotlin/default/dependencies.kt
+++ buildSrc/src/main/kotlin/default/dependencies.kt
@@ -219,7 +219,7 @@ interface TmsCommonLibDefaults: DependencyGroup {
 
 interface TmsKafkaToolsDefaults: DependencyGroup {
     override val groupId get() = "no.nav.tms.kafka"
-    override val version get() = "2.1.0"
+    override val version get() = "2.1.1-test"
 
     val kafkaApplication get() = dependency("kafka-application")
 }
