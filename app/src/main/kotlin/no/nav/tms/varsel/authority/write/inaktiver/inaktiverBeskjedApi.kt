ed31dded3d35198070d2caab6946eced617fba36
Christopher Santana <christopher.santana@nav.no>
2025-04-01 11:01:00 +0200

    DEBUG.

--- app/src/main/kotlin/no/nav/tms/varsel/authority/write/inaktiver/inaktiverBeskjedApi.kt
+++ app/src/main/kotlin/no/nav/tms/varsel/authority/write/inaktiver/inaktiverBeskjedApi.kt
@@ -1,12 +1,15 @@
 package no.nav.tms.varsel.authority.write.inaktiver
 
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.ApplicationCall
-import io.ktor.server.request.receive
+import io.ktor.server.request.*
 import io.ktor.server.response.respond
 import io.ktor.server.routing.*
 import no.nav.tms.token.support.tokenx.validation.user.TokenXUserFactory
 import no.nav.tms.varsel.action.VarselIdValidator
+import no.nav.tms.varsel.authority.config.defaultObjectMapper
 
 fun Route.inaktiverBeskjedApi(
     varselUpdater: VarselInaktiverer
@@ -18,8 +21,16 @@ fun Route.inaktiverBeskjedApi(
     }
 }
 
+private val securelog = KotlinLogging.logger("secureLog")
+
+private val localMapper = defaultObjectMapper()
+
 private suspend fun ApplicationCall.varselId(): String {
-    val requestBody = receive<VarselIdBody>()
+
+    val body = receiveText()
+    securelog.info { "Request body: $body" }
+
+    val requestBody = localMapper.readValue<VarselIdBody>(body)
 
     if (requestBody.varselId == null) {
         throw VarselIdMissingException()
