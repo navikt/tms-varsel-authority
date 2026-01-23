f770ea858aabe10d776a9924ccd3a53eed432f2c
Christopher Santana <christopher.santana@nav.no>
2026-01-23 15:01:41 +0100

    Egen jdbc-url for eldre cloudsql instanser.

--- app/src/main/kotlin/no/nav/tms/varsel/authority/config/Environment.kt
+++ app/src/main/kotlin/no/nav/tms/varsel/authority/config/Environment.kt
@@ -4,7 +4,7 @@ import no.nav.tms.common.util.config.IntEnvVar.getEnvVarAsInt
 import no.nav.tms.common.util.config.StringEnvVar.getEnvVar
 
 data class Environment(
-    val jdbcUrl: String = getEnvVar("DB_JDBC_URL"),
+    val jdbcUrl: String = jdbcUrl(),
     val archivingThresholdDays: Int = getEnvVarAsInt("ARCHIVING_THRESHOLD"),
     val kafkaBrokers: String = getEnvVar("KAFKA_BROKERS"),
     val kafkaTruststorePath: String = getEnvVar("KAFKA_TRUSTSTORE_PATH"),
@@ -14,3 +14,12 @@ data class Environment(
     val internalVarselTopic: String = "min-side.brukervarsel-v1",
     val publicVarselTopic: String = "min-side.aapen-brukervarsel-v1"
 )
+
+private fun jdbcUrl(): String {
+    val host: String = getEnvVar("DB_HOST")
+    val name: String = getEnvVar("DB_DATABASE")
+    val user: String = getEnvVar("DB_USERNAME")
+    val password: String = getEnvVar("DB_PASSWORD")
+
+    return "jdbc:postgresql://${host}/$name?user=$user&password=$password"
+}
